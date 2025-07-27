8aee87b615a7cd4c79aa61283b616a71501948d7
bigg-vann <evanderpuye9@gmail.com>
2025-07-27 20:46:04 +0000

    Delete admin directory

--- admin/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>All rights reserved</b>
-    </div>
-    <strong>Copyright &copy; 2024 Project Group 19</a></strong>
-</footer>
