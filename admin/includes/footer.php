e0d0492daea1bd23c7e6055e97eec24432797644
bigg-vann <evanderpuye9@gmail.com>
2025-07-27 20:41:08 +0000

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2024 Project Group 19</a></strong>
+</footer>
